10fc203e348ac580be93cde62d6565a4ef0d0b67
JRobuschi <juanrobuschi5@gmail.com>
2023-01-12 20:37:05 -0300

    Add indexRoute and config whit server.js

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ require("dotenv").config({ path: "./.env" });
 const port = process.env.PORT || 3010;
 const cors = require("cors");
 
-const errorHandler = require("./middlewares/errorHandler");
-const indexRoutes = require("./routes/index.route.js");
+//const errorHandler = require("./middlewares/errorHandler");
+const indexRoutes = require("./routes/indexRoute.js");
 
 //mysql connections:
-require("./config/connections");
-const router = require("./route/index.route");
+//require("./config/connections");
 
 //Middlewares
 server.use(cors());
@@ -18,7 +17,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 //Route:
-server.use(errorHandler);
+//server.use(errorHandler);
 server.use(express.static(__dirname));
 server.use("/", indexRoutes);
 
