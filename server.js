4fe7b85dbc7ba51af7fbbaba8dedbc155a7b93f0
VictorM-Herrera <herreramaxi@yahoo.com>
2023-01-12 20:55:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const server = express();
-
-require("dotenv").config({ path: "./.env" });
 const port = process.env.PORT || 3010;
 const cors = require("cors");
 
-const errorHandler = require("./middlewares/errorHandler");
-const indexRoutes = require("./routes/index.route.js");
+// const errorHandler = require("./middlewares/errorHandler");
+// const indexRoutes = require("./routes/index.route.js");
 
 //mysql connections:
-require("./config/connections");
-const router = require("./route/index.route");
+// require("./config/connections");
+// const router = require("./route/index.route");
 
 //Middlewares
 server.use(cors());
@@ -18,9 +17,9 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 //Route:
-server.use(errorHandler);
-server.use(express.static(__dirname));
-server.use("/", indexRoutes);
+// server.use(errorHandler);
+// server.use(express.static(__dirname));
+// server.use("/", indexRoutes);
 
 server.listen(port, () => {
   console.log(`Server in the port: ${port}`);
