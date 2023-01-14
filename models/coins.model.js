688b81b450adbc5db3098874f18449fbdb8f7ea9
JRobuschi <juanrobuschi5@gmail.com>
2023-01-14 19:16:24 -0300

    Add coins.model and change in the connections whit the wallet.model

---
+++ models/coins.model.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/mysql.config");
+
+//const validateRequest = require('../middlewares/validateRequest');
+//const Joi = require('joi');
+
+export const Coins = sequelize.define(
+  "Coins",
+  {
+    coin_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      unique: true,
+    },
+    amount: {
+      type: Sequelize.DECIMAL,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
