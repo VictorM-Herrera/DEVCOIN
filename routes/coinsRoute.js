efaa818d4472e7e96d86862bba4447fd8e23b57b
JRobuschi <juanrobuschi5@gmail.com>
2023-01-16 20:54:03 -0300

    Add coinsRoute, and your connection whit indexRoute

--- routes/coinsRoute.js
+++ routes/coinsRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { required } = require("joi");
 const coins = express.Router();
 const coinsController = require("../controllers/coins.controller");
+const { ValidateCoins } = require("../models/coins.model");
 
 coins.get("/", coinsController.getAllCoins);
-coins.post("/", coinsController.createCoins);
+coins.post("/", ValidateCoins, coinsController.createCoins);
 coins.get("/:name", coinsController.getByCoinName);
 coins.put("/:name", coinsController.UpdateCoins);
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,14 +2,17 @@
 //const userRouter = require('./user.route');
 const transactionRoute = require("./transactionRoute");
 const walletRoute = require("./walletRoute");
+const coinsRoute = require("./coinsRoute");
 
 //express:
 const express = require("express");
+const { required } = require("joi");
 const indexRouter = express.Router();
 
 //Uso de las rutas:
 //indexRouter.use('/users', userRouter);
 indexRouter.use("/transaction", transactionRoute);
 indexRouter.use("/wallet", walletRoute);
+indexRouter.use("/coins", coinsRoute);
 
 module.exports = indexRouter;
