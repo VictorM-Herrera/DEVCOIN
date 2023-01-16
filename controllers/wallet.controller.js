33fd76a9307b916690da19a79529fe8c8a8e497d
JRobuschi <juanrobuschi5@gmail.com>
2023-01-16 19:36:00 -0300

    Create wallet.controller.js

---
+++ controllers/wallet.controller.js
@@ -0,0 +1,22 @@
+const { Wallet } = require("../models/wallet.model");
+const walletController = {};
+
+walletController.getWalletByHexacode = async (req, res) => {
+  try {
+    const { hex_code } = req.params;
+    const response = await Wallet.findOne({ where: { hex_code: hex_code } })
+      .then((data) => {
+        const res = { error: false, data: data };
+        return res;
+      })
+      .catch((error) => {
+        const res = { error: true, message: error };
+        return res;
+      });
+    res.json(response);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = walletController;
