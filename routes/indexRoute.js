34f0d9144f85cd985c7b94a37da35272ca5bb4e6
JRobuschi <juanrobuschi5@gmail.com>
2023-01-16 12:21:02 -0300

    Update indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,5 +1,6 @@
 //requires delas rutas:
 //const userRouter = require('./user.route');
+const transactionRoute = require("./transactionRoute");
 
 //express:
 const express = require("express");
@@ -7,5 +8,6 @@ const indexRouter = express.Router();
 
 //Uso de las rutas:
 //indexRouter.use('/users', userRouter);
+indexRouter.use("/transaction", transactionRoute);
 
 module.exports = indexRouter;
