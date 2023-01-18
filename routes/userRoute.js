cb654251e0d252a0f6a28ed49bad396833d49b82
VictorM-Herrera <herreramaxi@yahoo.com>
2023-01-18 12:11:06 -0300

    Test OK

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,18 +4,20 @@ const userController = require('../controllers/user.controller');
 const { ValidateUser } = require('../models/user.model');
 
 //gets:
-router.get('/', userController.getAllUsers);
-router.get('/:hexCode', userController.getUserByHexCode);
-router.get('/recover', userController.sendRecoverMail);
+router.get('/', userController.getAllUsers);//funciona
+router.get('/:hexCode', userController.getUserByHexCode);//funciona
+// router.get('/activeOnly', userController.getAllUsersActive);
+
 
 //Posts
-router.post('/', [ValidateUser],userController.createUser);
-router.post('/logIn/', userController.logInUser);
+router.post('/', [ValidateUser],userController.createUser);//funciona
+router.post('/logIn/', userController.logInUser);//funciona
+router.post('/recover', userController.sendRecoverMail);//funciona
 
 //updates
-router.put('/:hexCode', userController.updateUserByHexCode);
-router.patch('/:email', userController.verifyAccount);
-router.put('/recoverPass/:email', userController.recoverPassword);
+router.put('/:hexCode', userController.updateUserByHexCode);//funciona
+router.patch('/:email', userController.verifyAccount);//funciona
+router.put('/recoverPass/:email', userController.recoverPassword);//funciona
 
 //deletes
 router.delete('/:hexCode', userController.deleteUserByHexCode);
