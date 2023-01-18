bd7f5a6f0f466aaf054cbab9011b082173eb8efe
JRobuschi <juanrobuschi5@gmail.com>
2023-01-18 12:43:37 -0300

    Update transactions.model.js

--- models/transactions.model.js
+++ models/transactions.model.js
@@ -1,65 +1,70 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../config/mysql.config");
-const Joi = require('joi');
-const validateRequest = require('../middlewares/validateRequest')
+const Joi = require("joi");
+const validateRequest = require("../middlewares/validateRequest");
 
-const Transaction = sequelize.define("Transactions", {
+const Transaction = sequelize.define(
+  "Transactions",
+  {
+    transaction_id: {
+      // ID DE TRANSACCION
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-  transaction_id: {         // ID DE TRANSACCION
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  }, 
+    transaction_date: {
+      // FECHA DE TRANSACCION
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn("NOW"),
+    },
 
-  transaction_date: {       // FECHA DE TRANSACCION 
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.fn('NOW'),
-  }, 
+    sender_hexcode: {
+      // CODIGO DEL EMISOR
+      type: Sequelize.STRING,
+    },
 
-  sender_hexcode: {         // CODIGO DEL EMISOR 
-    type: Sequelize.STRING,
+    receiver_hexcode: {
+      // CODIGO DEL RECEPTOR
+      type: Sequelize.STRING,
+    },
+
+    amount: {
+      // CANTIDAD - TIPO DE VARIABLE : DECIMAL
+      type: Sequelize.DOUBLE,
+    },
+
+    type_coin: {
+      // TIPO DE CRYTOMONEDA
+      type: Sequelize.STRING,
+    },
   },
+  { timestamps: false }
+);
 
-  receiver_hexcode: {       // CODIGO DEL RECEPTOR
-    type: Sequelize.STRING,
-  },
-
-  amount: {                 // CANTIDAD - TIPO DE VARIABLE : DECIMAL
-    type: Sequelize.DOUBLE,
-  },
-
-  type_coin: {              // TIPO DE CRYTOMONEDA
-    type: Sequelize.STRING,
-  },
-},{timestamps:false});
-
-const ValidateTransaction = (req,res,next) => {
-    const schema = Joi.object({
-        sender_hexcode: Joi.string().required()
-        .messages({
-            'string.empty': "Ingresa al Emisor",
-            'any.required': "Ingresa al Emisor"
-        }),
-        receiver_hexcode: Joi.string().required()
-        .messages({
-            'string.empty': "Ingresa al Receptor",
-            'any.required': "Ingresa al Receptor"
-        }),
-        amount: Joi.number().required()
-        .messages({
-            'number.empty': "Ingrese el monto a transferir",
-            'any.required': "Ingrese el monto a transferir"
-        }),
-        type_coin: Joi.string().required()
-        .messages({
-            'string.empty': "Ingresa la cryptomoneda",
-            'any.required': "Ingresa al cryptomoneda"
-        }),
-    });
-    validateRequest(req,res,next,schema);
-}
+const ValidateTransaction = (req, res, next) => {
+  const schema = Joi.object({
+    sender_hexcode: Joi.string().required().messages({
+      "string.empty": "Ingresa al Emisor",
+      "any.required": "Ingresa al Emisor",
+    }),
+    receiver_hexcode: Joi.string().required().messages({
+      "string.empty": "Ingresa al Receptor",
+      "any.required": "Ingresa al Receptor",
+    }),
+    amount: Joi.number().required().messages({
+      "number.empty": "Ingrese el monto a transferir",
+      "any.required": "Ingrese el monto a transferir",
+    }),
+    type_coin: Joi.string().required().messages({
+      "string.empty": "Ingresa la cryptomoneda",
+      "any.required": "Ingresa al cryptomoneda",
+    }),
+  });
+  validateRequest(req, res, next, schema);
+};
 
 module.exports = {
-    Transaction,
-    ValidateTransaction
-}
+  Transaction,
+  ValidateTransaction,
+};
