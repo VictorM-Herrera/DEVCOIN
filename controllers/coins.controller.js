b42457652a0197a9bcc6ca7f05ef158df71d7057
VictorM-Herrera <herreramaxi@yahoo.com>
2023-01-27 11:41:47 -0300

    update coin create
    se modularizo la funcion de comprar moneditas

--- controllers/coins.controller.js
+++ controllers/coins.controller.js
@@ -4,61 +4,66 @@ const { Coins } = require("../models/coins.model");
 const { Wallet } = require("../models/wallet.model");
 const coinsController = {};
 
+const findWalletId = async (req, res) => {
+  const walletId = Wallet.findOne({
+    where: { hexacode_user: req.body.hexacode },
+  })
+  .then((data) => {
+    return data.dataValues.wallet_id;
+  })
+  return walletId;
+}
+
+const findWalletBalance = async (req, res) => {
+  const walletBalance = Wallet.findOne({
+    where: { hexacode_user: req.body.hexacode },
+  })
+  .then((data) => {
+    return data.dataValues.balance;
+  })
+  return walletBalance;
+}
+
+const updateWalletBalance = async (req, res, total) => {
+  const updateWallet = Wallet.update(
+  { balance: total },{ 
+    where: { hexacode_user: req.body.hexacode } 
+  });
+  return updateWallet;
+}
+
+
 coinsController.createCoins = async (req, res) => {
   try {
     let flag = true;
-    const walletId = await Wallet.findOne({
-      where: { hexacode_user: req.body.hexacode },
-    })
-      .then((data) => {
-        return data.dataValues.wallet_id;
-      })
-      .catch((err) => {
-        flag = false;
-        res.status(400).json({ error: true, message: err });
-      });
+    const walletId = await findWalletId(req, res);
     const aux = await Coins.findOne({
       where: { symbol: req.body.symbol, walletId: walletId },
     }).then(async (data) => {
-      if (data) {
-        const walletBalance = await Wallet.findOne({
-          where: { hexacode_user: req.body.hexacode },
-        }).then(async (data) => {
-          if (data.dataValues.balance < req.body.total) {
-            flag = false;
-            res.status(409).json({ message: "Saldo insuficiente" });
-          } else {
-            let total =
-              parseFloat(data.dataValues.balance) - parseFloat(req.body.total);
-
-            const updateWallet = await Wallet.update(
-              {
-                balance: total,
-              },
-              { where: { hexacode_user: req.body.hexacode } }
-            );
-            return updateWallet;
-          }
-        });
+      if (data) {//SI LA MONEDA EXISTE
+        const walletBalance = await findWalletBalance(req,res);
+        if (walletBalance < req.body.total) {
+          flag=false;
+          res.status(409).json({ message: "Saldo insuficiente"});
+        }else{
+          let total = parseFloat(walletBalance) - parseFloat(req.body.total);
+          console.log("(YA EXISTE) Se resta "+ req.body.total+" a "+ walletBalance + " y da como resultado: "+ total);
+          updateWalletBalance(req, res, total);
+        }
         if (flag !== false) {
-          const addAmount =
-            parseFloat(data.dataValues.amount) + parseFloat(req.body.amount);
-          const add = await Coins.update(
-            { amount: addAmount },
-            { where: { coin_id: data.dataValues.coin_id } }
-          )
-            .then((data) => {
+          const addAmount = parseFloat(data.dataValues.amount) + parseFloat(req.body.amount);
+          const add = await Coins.update({ amount: addAmount },{ 
+            where: { coin_id: data.dataValues.coin_id } 
+          }).then((data) => {
               const res = { data: data, message: "Saldo actualizado" };
               return res;
-            })
-            .catch((err) => {
+            }).catch((err) => {
               flag = false;
               res.status(400).json({ error: true, message: err });
             });
-
           return add;
         }
-      } else {
+      } else {//SI LA MONEDA NO EXISTE:
         const modelCoin = {
           name: req.body.name,
           symbol: req.body.symbol,
@@ -67,56 +72,26 @@ coinsController.createCoins = async (req, res) => {
           walletId: walletId,
         };
         //ACA VA LA VERIFICACION DEL SALDO (WALLET):
-        const walletBalance = await Wallet.findOne({
-          where: { hexacode_user: req.body.hexacode },
-        }).then(async (data) => {
-          if (data.dataValues.balance < req.body.total) {
-            flag = false;
-            res.status(409).json({ message: "Saldo insuficiente" });
-          } else {
-            let total =
-              parseFloat(data.dataValues.balance) - parseFloat(req.body.total);
-
-            const updateWallet = await Wallet.update(
-              {
-                balance: total,
-              },
-              { where: { hexacode_user: req.body.hexacode } }
-            );
-            return updateWallet;
-          }
-        });
+        const walletBalance = await findWalletBalance(req, res);
+        if(walletBalance < req.body.total)
+        {
+          flag = false;
+          res.status(409).json({ message: "Saldo insuficiente" });
+        }else{
+          let total =  parseFloat(walletBalance) - parseFloat(req.body.total);
+          console.log("(SE CREA LA COIN) Se resta "+ req.body.total+" a "+ walletBalance + " y da como resultado: "+ total);
+          updateWalletBalance(req, res, total);
+        }
         if (flag !== false) {
           const response = await Coins.create(modelCoin)
             .then(async (data) => {
-              let total =
-                parseFloat(data.dataValues.balance) -
-                parseFloat(req.body.total);
-
-              const updateWallet = await Wallet.update(
-                {
-                  balance: total,
-                },
-                { where: { hexacode_user: req.body.hexacode } }
-              );
-
               return {
-                error: false,
-                data: data,
-                message: "Coin creada",
-                update: updateWallet,
-              };
+                error: false, data: data, message: "Coin creada"};
             })
             .catch((e) => {
               flag = false;
-              if (
-                e.name == "SequelizeUniqueConstraintError" ||
-                e.name == "SequelizeValidationError"
-              ) {
-                res.status(409).json({
-                  error: true,
-                  message: e.errors.map((err) => err.message),
-                });
+              if (e.name == "SequelizeUniqueConstraintError" || e.name == "SequelizeValidationError") {
+                res.status(409).json({ error: true, message: e.errors.map((err) => err.message)});
               } else {
                 res.status(409).json({ error: true, message: e });
               }
@@ -125,10 +100,12 @@ coinsController.createCoins = async (req, res) => {
         }
       }
     });
+    //final de la funcion
     if (flag === false) {
     } else {
       res.json(aux);
     }
+
   } catch (e) {
     console.log(e);
   }
@@ -170,16 +147,7 @@ coinsController.getByCoinSymbol = async (req, res) => {
 coinsController.sellCoins = async (req, res) => {
   try {
     let flag = true;
-    const walletId = await Wallet.findOne({
-      where: { hexacode_user: req.body.hexacode },
-    })
-      .then((data) => {
-        return data.dataValues.wallet_id;
-      })
-      .catch((err) => {
-        flag = false;
-        res.status(400).json({ error: true, message: err });
-      });
+    const walletId = await findWalletId(req, res);
     const modelCoin = {
       symbol: req.body.symbol,
       amount: req.body.amount,
@@ -212,25 +180,10 @@ coinsController.sellCoins = async (req, res) => {
             }
           )
             .then(async (data) => {
-              const walletBalance = await Wallet.findOne({
-                where: { hexacode_user: req.body.hexacode },
-              }).then(async (data) => {
-                let total =
-                  parseFloat(data.dataValues.balance) +
-                  parseFloat(req.body.total);
-                const updateWallet = await Wallet.update(
-                  {
-                    balance: total,
-                  },
-                  { where: { hexacode_user: req.body.hexacode } }
-                );
-                return updateWallet;
-              });
-              const res = {
-                error: false,
-                data: data,
-                message: "Saldo y balance actualizado",
-              };
+              const walletBalance = await findWalletBalance(req,res);
+              let total = parseFloat(walletBalance) + parseFloat(req.body.total);
+              updateWalletBalance(req,res,total);
+              const res = { error: false, data: data, message: "Saldo y balance actualizado" };
               return res;
             })
             .catch((err) => {
@@ -242,6 +195,7 @@ coinsController.sellCoins = async (req, res) => {
         }
       }
     });
+
     if (flag === false) {
     } else {
       res.json({ validate: validate });
