373cffeaf3e0bf5e4fb113db800ba94f9d401d0d
JRobuschi <juanrobuschi5@gmail.com>
2023-01-30 12:36:18 -0300

    Update coins.controller.js

--- controllers/coins.controller.js
+++ controllers/coins.controller.js
@@ -7,31 +7,30 @@ const coinsController = {};
 const findWalletId = async (req, res) => {
   const walletId = Wallet.findOne({
     where: { hexacode_user: req.body.hexacode },
-  })
-  .then((data) => {
+  }).then((data) => {
     return data.dataValues.wallet_id;
-  })
+  });
   return walletId;
-}
+};
 
 const findWalletBalance = async (req, res) => {
   const walletBalance = Wallet.findOne({
     where: { hexacode_user: req.body.hexacode },
-  })
-  .then((data) => {
+  }).then((data) => {
     return data.dataValues.balance;
-  })
+  });
   return walletBalance;
-}
+};
 
 const updateWalletBalance = async (req, res, total) => {
   const updateWallet = Wallet.update(
-  { balance: total },{ 
-    where: { hexacode_user: req.body.hexacode } 
-  });
+    { balance: total },
+    {
+      where: { hexacode_user: req.body.hexacode },
+    }
+  );
   return updateWallet;
-}
-
+};
 
 coinsController.createCoins = async (req, res) => {
   try {
@@ -40,30 +39,45 @@ coinsController.createCoins = async (req, res) => {
     const aux = await Coins.findOne({
       where: { symbol: req.body.symbol, walletId: walletId },
     }).then(async (data) => {
-      if (data) {//SI LA MONEDA EXISTE
-        const walletBalance = await findWalletBalance(req,res);
+      if (data) {
+        //SI LA MONEDA EXISTE
+        const walletBalance = await findWalletBalance(req, res);
         if (walletBalance < req.body.total) {
-          flag=false;
-          res.status(409).json({ message: "Saldo insuficiente"});
-        }else{
+          flag = false;
+          res.status(409).json({ message: "Saldo insuficiente" });
+        } else {
           let total = parseFloat(walletBalance) - parseFloat(req.body.total);
-          console.log("(YA EXISTE) Se resta "+ req.body.total+" a "+ walletBalance + " y da como resultado: "+ total);
+          console.log(
+            "(YA EXISTE) Se resta " +
+              req.body.total +
+              " a " +
+              walletBalance +
+              " y da como resultado: " +
+              total
+          );
           updateWalletBalance(req, res, total);
         }
         if (flag !== false) {
-          const addAmount = parseFloat(data.dataValues.amount) + parseFloat(req.body.amount);
-          const add = await Coins.update({ amount: addAmount },{ 
-            where: { coin_id: data.dataValues.coin_id } 
-          }).then((data) => {
+          const addAmount =
+            parseFloat(data.dataValues.amount) + parseFloat(req.body.amount);
+          const add = await Coins.update(
+            { amount: addAmount },
+            {
+              where: { coin_id: data.dataValues.coin_id },
+            }
+          )
+            .then((data) => {
               const res = { data: data, message: "Saldo actualizado" };
               return res;
-            }).catch((err) => {
+            })
+            .catch((err) => {
               flag = false;
               res.status(400).json({ error: true, message: err });
             });
           return add;
         }
-      } else {//SI LA MONEDA NO EXISTE:
+      } else {
+        //SI LA MONEDA NO EXISTE:
         const modelCoin = {
           name: req.body.name,
           symbol: req.body.symbol,
@@ -73,25 +87,40 @@ coinsController.createCoins = async (req, res) => {
         };
         //ACA VA LA VERIFICACION DEL SALDO (WALLET):
         const walletBalance = await findWalletBalance(req, res);
-        if(walletBalance < req.body.total)
-        {
+        if (walletBalance < req.body.total) {
           flag = false;
           res.status(409).json({ message: "Saldo insuficiente" });
-        }else{
-          let total =  parseFloat(walletBalance) - parseFloat(req.body.total);
-          console.log("(SE CREA LA COIN) Se resta "+ req.body.total+" a "+ walletBalance + " y da como resultado: "+ total);
+        } else {
+          let total = parseFloat(walletBalance) - parseFloat(req.body.total);
+          console.log(
+            "(SE CREA LA COIN) Se resta " +
+              req.body.total +
+              " a " +
+              walletBalance +
+              " y da como resultado: " +
+              total
+          );
           updateWalletBalance(req, res, total);
         }
         if (flag !== false) {
           const response = await Coins.create(modelCoin)
             .then(async (data) => {
               return {
-                error: false, data: data, message: "Coin creada"};
+                error: false,
+                data: data,
+                message: "Coin creada",
+              };
             })
             .catch((e) => {
               flag = false;
-              if (e.name == "SequelizeUniqueConstraintError" || e.name == "SequelizeValidationError") {
-                res.status(409).json({ error: true, message: e.errors.map((err) => err.message)});
+              if (
+                e.name == "SequelizeUniqueConstraintError" ||
+                e.name == "SequelizeValidationError"
+              ) {
+                res.status(409).json({
+                  error: true,
+                  message: e.errors.map((err) => err.message),
+                });
               } else {
                 res.status(409).json({ error: true, message: e });
               }
@@ -105,7 +134,6 @@ coinsController.createCoins = async (req, res) => {
     } else {
       res.json(aux);
     }
-
   } catch (e) {
     console.log(e);
   }
@@ -180,13 +208,19 @@ coinsController.sellCoins = async (req, res) => {
             }
           )
             .then(async (data) => {
-              const walletBalance = await findWalletBalance(req,res);
-              let total = parseFloat(walletBalance) + parseFloat(req.body.total);
-              updateWalletBalance(req,res,total);
-              const res = { error: false, data: data, message: "Saldo y balance actualizado" };
-              return res;
+              const walletBalance = await findWalletBalance(req, res);
+              let total =
+                parseFloat(walletBalance) + parseFloat(req.body.total);
+              updateWalletBalance(req, res, total);
+              const resp = {
+                error: false,
+                data: data,
+                message: "Saldo y balance actualizado",
+              };
+              return resp;
             })
             .catch((err) => {
+              console.log(err);
               flag = false;
               res.status(401).json({ error: true, message: err });
             });
